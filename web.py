3e71fb8c39418af447055e2d8c1cd44fa8cf427a
shyamsswaroop <shyam125@gmail.com>
2016-12-05 16:48:29 +0530

    Update web.py

--- web.py
+++ web.py
@@ -1,7 +1,12 @@
+import os
 from flask import Flask
-app = Flask(__name__)
-app.run(debug=True, port=33507)
 
-@app.route('/')
-def index():
-  return 'hello world'
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello world!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
