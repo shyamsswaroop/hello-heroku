d5856a086585777f9b996548d1d23f130f1510a9
shyamsswaroop <shyam125@gmail.com>
2016-12-07 14:32:02 +0530

    Create tests.py

---
+++ tests.py
@@ -0,0 +1,14 @@
+import unittest
+import web
+
+class FlaskrTestCase(unittest.Testcase):
+  def setUp (self):
+    self.app = web.app.test_client()
+    
+  def test_index (self):
+    rv = self.app.get('/')
+    assert b'hello world' in rv.data
+    
+if __name__ == '__main__' :
+unittest.main()
+    
